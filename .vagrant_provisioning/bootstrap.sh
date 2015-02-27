cd7638aedea30972d87b9aa01bb32264c7623dce
whiter <rob.white@truelocal.com.au>
2015-02-27 17:31:35 +1100

    Minor bug fixes on vagrant provisioning process

--- .vagrant_provisioning/bootstrap.sh
+++ .vagrant_provisioning/bootstrap.sh
@@ -4,7 +4,7 @@
 yum -y install ansible sshpass python-boto python-netaddr git
 
 # Copy private key so Ansible can access itself
-cp /vagrant/machines/default/virtualbox/private_key /root/.ssh/id_rsa
+cp /vagrant/.vagrant/machines/default/virtualbox/private_key /home/vagrant/.ssh/id_rsa
 
 # Run the playbook to configure everything else
 ansible-playbook /vagrant/.vagrant_provisioning/deploy_role.yml -i /vagrant/.vagrant_provisioning/inventories/local --extra-vars "role=ansible user=vagrant"
