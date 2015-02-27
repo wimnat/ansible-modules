291ba607b48c1c6e358bd3d029344a04c4e93e47
whiter <rob.white@truelocal.com.au>
2015-02-27 17:13:17 +1100

    Added Vagrant setup

---
+++ .vagrant_provisioning/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# Install ansible and other necessary packages
+yum -y install ansible sshpass python-boto python-netaddr git
+
+# Copy private key so Ansible can access itself
+cp /vagrant/machines/default/virtualbox/private_key /root/.ssh/id_rsa
+
+# Run the playbook to configure everything else
+ansible-playbook /vagrant/.vagrant_provisioning/deploy_role.yml -i /vagrant/.vagrant_provisioning/inventories/local --extra-vars "role=ansible user=vagrant"
