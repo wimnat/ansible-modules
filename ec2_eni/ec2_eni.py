dade37359f9a72dc68e99342d1c433c9725fbc01
whiter <rob.white@truelocal.com.au>
2015-04-15 17:36:35 +1000

    Added modify and delete functionality

--- ec2_eni/ec2_eni.py
+++ ec2_eni/ec2_eni.py
@@ -12,16 +12,15 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
-from _elementtree import ElementTree
 
 DOCUMENTATION = '''
 ---
 module: ec2_eni
 short_description: Create and optionally attach an Elastic Network Interface (ENI) to an instance
 description:
-    - Create and optionally attach an Elastic Network Interface (ENI) to an instance
+    - Create and optionally attach an Elastic Network Interface (ENI) to an instance. If an ENI ID is provided, an attempt is made to update the existing ENI. By passing 'None' as the instance_id, an ENI can be detached from an instance.
 version_added: "1.9"
-author: Mohammed Salih
+author: Rob White
 options:
   instance_id:
     description:
@@ -64,23 +63,59 @@ options:
     required: false
     default: null
     aliases: []
+  force_detach:
+    description:
+      - Force detachment of the interface. This applies either when explicitly detaching the interface by setting instance_id to None or when deleting an interface with state=absent
+    required: false
+    default: no
 extends_documentation_fragment: aws
 requirements: [ "boto" ]
 '''
 
 EXAMPLES = '''
-# Create and attach action.
-- local_action: 
-    module: ec2_eni
-    instance: i-xxxxxxxx
+# Create an ENI. As no security group is defined, ENI will be created in default security group
+- ec2_eni:
     private_ip_address: 172.31.0.20
     subnet_id: subnet-xxxxxxxx
+    state: present
+
+# Create an ENI and attach it to an instance
+- ec2_eni:
+    instance_id: i-xxxxxxx
+    device_index: 1
+    private_ip_address: 172.31.0.20
+    subnet_id: subnet-xxxxxxxx
+    state: present
+    
+# Destroy an ENI, detaching it from any instance if necessary
+- ec2_eni:
+    eni_id: eni-xxxxxxx
+    force_detach: yes
+    state: absent
+    
+# Update an ENI
+- ec2_eni:
+    eni_id: eni-xxxxxxx
+    description: "My new description"
+    state: present
+    
+# Detach an ENI from an instance
+- ec2_eni:
+    eni_id: eni-xxxxxxx
+    instance_id: None
+    state: present
+    
+# List all ENIs
+- ec2_eni:
+    state: list
+
 
 '''
 
 import sys
 import time
-import xml
+import xml.etree.ElementTree as ET
+import re
 
 from ansible.module_utils.basic import *
 from ansible.module_utils.ec2 import *
@@ -92,6 +127,13 @@ except ImportError:
     print "failed=True msg='boto required for this module'"
     sys.exit(1)
 
+def get_error_message(xml_string):
+    
+    root = ET.fromstring(xml_string)
+    for message in root.iter('Message'):            
+        return message.text
+    
+    
 def get_eni_info(interface):
     
     interface_info = {'id': interface.id,
@@ -120,8 +162,9 @@ def get_eni_info(interface):
     
 def create_eni(connection, module):
     
-    eni_id = module.params.get("eni_id")
     instance_id = module.params.get("instance_id")
+    if instance_id == 'None':
+        instance_id = None
     device_index = module.params.get("device_index")
     subnet_id = module.params.get('subnet_id')
     private_ip_address = module.params.get('private_ip_address')
@@ -130,56 +173,160 @@ def create_eni(connection, module):
     changed = False
     
     try:
-        eni = connection.create_network_interface(subnet_id, private_ip_address, description, security_groups)
-        changed = True
-    except BotoServerError as ex:
-        #module.fail_json(msg=str(e))
-        template = "An exception of type {0} occured. Arguments:\n{1!r}"
-        message = template.format(type(ex).__name__, ex.args)
-        print message
-        print ex.args[2]
-        
-        #tree = ElementTree.fromstring()
-        
-        root = xml.ElementTree.fromstring(ex.args[2])
-        #e = xml.etree.ElementTree.parse(ex.args[2]).getroot()
-        print root.response.errors.error.message
-        
-    module.exit_json(changed=changed, id=eni.id, subnet_id=eni.subnet_id)
+        eni = compare_eni(connection, module)
+        if eni is None:            
+            eni = connection.create_network_interface(subnet_id, private_ip_address, description, security_groups)
+            if instance_id is not None:
+                try:
+                    eni.attach(instance_id, device_index)
+                except BotoServerError as ex:
+                    eni.delete()
+                    raise
+            changed = True
+            # Wait to allow creation / attachment to finish
+            time.sleep(3)
+            eni.update()
+            
+    except BotoServerError as e:
+        module.fail_json(msg=get_error_message(e.args[2]))
+                
+    module.exit_json(changed=changed, interface=get_eni_info(eni))
+    
+
+def modify_eni(connection, module):
+    
+    eni_id = module.params.get("eni_id")
+    instance_id = module.params.get("instance_id")
+    if instance_id == 'None':
+        instance_id = None
+    device_index = module.params.get("device_index")
+    subnet_id = module.params.get('subnet_id')
+    private_ip_address = module.params.get('private_ip_address')
+    description = module.params.get('description')
+    security_groups = module.params.get('security_groups')
+    force_detach = module.params.get("force_detach")
+    changed = False
+    
+    try:
+        eni = compare_eni(connection, module)
+        if eni is None:
+            # Get the eni_id specified
+            eni_result_set = connection.get_all_network_interfaces(eni_id)
+            eni = eni_result_set[0]
+            if description is not None:
+                if eni.description != description:
+                    connection.modify_network_interface_attribute(eni.id, "description", description)
+                    changed = True
+            if security_groups is not None:
+                if sorted(get_sec_group_list(eni.groups)) != sorted(security_groups):
+                    connection.modify_network_interface_attribute(eni.id, "groupSet", security_groups)
+                    changed = True
+            if eni.attachment is not None:
+                if eni.attachment.instance_id != instance_id or eni.attachment.device_index != device_index:
+                    eni.detach(force_detach)
+                    if instance_id is not None:
+                        eni.attach(instance_id, device_index)
+                    changed = True
+            else:
+                if instance_id is not None:
+                    eni.attach(instance_id, device_index)
+                    changed = True
+
+    except BotoServerError as e:
+        module.fail_json(msg=get_error_message(e.args[2]))
+                
+    eni.update()
+    module.exit_json(changed=changed, interface=get_eni_info(eni))
+    
     
 def delete_eni(connection, module):
     
+    eni_id = module.params.get("eni_id")
+    force_detach = module.params.get("force_detach")
+    
     try:
-        eni_id = "" 
-        eni = ec2.delete_network_interface(eni_id)
-        changed = True
-    except BotoServerError, e:
-        module.fail_json(msg=str(e))
+        eni_result_set = connection.get_all_network_interfaces(eni_id)
+        eni = eni_result_set[0]
+        
+        if force_detach is True:
+            if eni.attachment is not None:
+                eni.detach(force_detach)
+            eni.delete()
+            changed = True
+        else:
+            eni.delete()
+            changed = True
+        
+        module.exit_json(changed=changed)
+    except BotoServerError as e:
+        msg = get_error_message(e.args[2])
+        regex = re.compile('The networkInterface ID \'.*\' does not exist')
+        if regex.search(msg) is not None:
+            module.exit_json(changed=False)
+        else:
+            module.fail_json(msg=get_error_message(e.args[2]))
         
 
 def list_eni(connection, module):
     
+    eni_id = module.params.get("eni_id")
     interface_dict_array = []
-    all_eni = connection.get_all_network_interfaces()
+    
+    try:
+        all_eni = connection.get_all_network_interfaces(eni_id)
+    except BotoServerError as e:
+        module.fail_json(msg=get_error_message(e.args[2]))
     
     for interface in all_eni:
         interface_dict_array.append(get_eni_info(interface))
         
     module.exit_json(changed=False, interfaces=interface_dict_array)
+    
+def compare_eni(connection, module):
+    
+    eni_id = module.params.get("eni_id")
+    subnet_id = module.params.get('subnet_id')
+    private_ip_address = module.params.get('private_ip_address')
+    description = module.params.get('description')
+    security_groups = module.params.get('security_groups')
+    
+    try:
+        all_eni = connection.get_all_network_interfaces(eni_id)
+        eni = all_eni[0]
+        
+        remote_security_groups = get_sec_group_list(eni.groups)
+                
+        if (eni.subnet_id == subnet_id) and (eni.private_ip_address == private_ip_address) and (eni.description == description) and (remote_security_groups == security_groups): 
+            return eni
+    
+    except BotoServerError as e:
+        module.fail_json(msg=get_error_message(e.args[2]))
+        
+    return None
+
+def get_sec_group_list(groups):
+    
+    # Build list of remote security groups
+    remote_security_groups = []
+    for group in groups:
+        remote_security_groups.append(group.id.encode())
+        
+    return remote_security_groups
 
 
 def main():
     argument_spec = ec2_argument_spec()
     argument_spec.update(
         dict(
-            eni_id = dict(),
+            eni_id = dict(default=None),
             instance_id = dict(),
             private_ip_address = dict(),
             subnet_id = dict(),
             description = dict(),
-            security_groups = dict(),           
-            device_index = dict(),
-            state=dict(default='present', choices=['present', 'absent', 'list'])
+            security_groups = dict(type='list'),           
+            device_index = dict(default=0, type='int'),
+            state=dict(default='present', choices=['present', 'absent', 'list']),
+            force_detach=dict(default='no', type='bool')
         )
     )
     
@@ -187,86 +334,30 @@ def main():
     
     region, ec2_url, aws_connect_params = get_aws_connection_info(module)
     
-    try:
-        connection = connect_to_aws(boto.ec2, region, **aws_connect_params)
-    except (boto.exception.NoAuthHandlerFound, StandardError), e:
-        module.fail_json(msg=str(e))
+    if region:
+        try:
+            connection = connect_to_aws(boto.ec2, region, **aws_connect_params)
+        except (boto.exception.NoAuthHandlerFound, StandardError), e:
+            module.fail_json(msg=str(e))
+    else:
+        module.fail_json(msg="region must be specified")
 
-    state = module.params.get('state')
+    state = module.params.get("state")
+    eni_id = module.params.get("eni_id")
 
     if state == 'present':
-        create_eni(connection, module)
+        if eni_id is None:
+            create_eni(connection, module)
+        else:
+            modify_eni(connection, module)
     elif state == 'absent':
-        delete_eni(connection, module)
+        if eni_id is None:
+            module.fail_json(msg="eni_id must be specified")
+        else:
+            delete_eni(connection, module)      
     elif state == 'list':
         list_eni(connection, module)
         
-    module.fail_json(msg="got to end")
-
-    instance = module.params.get('instance')
-    private_ip_address = module.params.get('private_ip_address')
-    subnet_id = module.params.get('subnet_id')
-    device_index = module.params.get('device_index')
-    sg_group = module.params.get('sg_group')
-    description = module.params.get('description')
-    region = module.params.get('region')
-    zone = module.params.get('zone')
-    ec2_url = module.params.get('ec2_url')
-    ec2_secret_key = module.params.get('ec2_secret_key')
-    ec2_access_key = module.params.get('ec2_access_key')
-
-    # allow eucarc environment variables to be used if ansible vars aren't set
-    if not ec2_url and 'EC2_URL' in os.environ:
-        ec2_url = os.environ['EC2_URL']
-    if not ec2_secret_key and 'EC2_SECRET_KEY' in os.environ:
-        ec2_secret_key = os.environ['EC2_SECRET_KEY']
-    if not ec2_access_key and 'EC2_ACCESS_KEY' in os.environ:
-        ec2_access_key = os.environ['EC2_ACCESS_KEY']
-    
-    # If we have a region specified, connect to its endpoint.
-    if region: 
-        try:
-            ec2 = boto.ec2.connect_to_region(region, aws_access_key_id=ec2_access_key, aws_secret_access_key=ec2_secret_key)
-        except boto.exception.NoAuthHandlerFound, e:
-            module.fail_json(msg = str(e))
-    # Otherwise, no region so we fallback to the old connection method
-    else: 
-        try:
-            if ec2_url: # if we have an URL set, connect to the specified endpoint 
-                ec2 = boto.connect_ec2_endpoint(ec2_url, ec2_access_key, ec2_secret_key)
-            else: # otherwise it's Amazon.
-                ec2 = boto.connect_ec2(ec2_access_key, ec2_secret_key)
-        except boto.exception.NoAuthHandlerFound, e:
-            module.fail_json(msg = str(e))
-
-    # If no instance supplied, try ENI creation based on module parameters.
-    sg_group = [sg_group]
-    try:
-        eni = ec2.create_network_interface(subnet_id, private_ip_address, description, sg_group)
-        nicfilter = {"network-interface-id" : eni.id}
-        while ec2.get_all_network_interfaces(nicfilter)[0].status != 'available':
-            time.sleep(3)
-    except boto.exception.BotoServerError, e:
-        module.fail_json(msg = "%s: %s" % (e.error_code, e.error_message))
-
-    # Attach the new ENI if instance is specified.
-
-    if instance:
-        reservation = ec2.get_all_instances(instance_ids=instance)
-        inst = reservation[0].instances[0]
-        zone = inst.placement
-        try:
-            attach = ec2.attach_network_interface(eni.id, inst.id, device_index)
-            while ec2.get_all_network_interfaces(nicfilter)[0].status != 'in-use':
-                time.sleep(3)
-        except boto.exception.BotoServerError, e:
-            module.fail_json(msg = "%s: %s" % (e.error_code, e.error_message))           
-   
-
-    print json.dumps({
-        "eni_id": eni.id
-    })
-    sys.exit(0)
 
 # this is magic, see lib/ansible/module_common.py
 #<<INCLUDE_ANSIBLE_MODULE_COMMON>>
